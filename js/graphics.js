e7e07f9923788098ab15f965c2f99a2b2a4d3fd4
Alex Lambert <alambe22@vols.utk.edu>
2019-09-28 00:25:40 -0400

    Added line drawing and rotation. Closed #2

--- js/graphics.js
+++ js/graphics.js
@@ -1,9 +1,45 @@
-function setup(){
-    draw_circle(50, 300, 300);
-    draw_circle(40, 200, 200);
-    draw_circle(30, 100, 100);
+class Circle{
+    constructor(radius, centerX, centerY, rotation){
+        this.radius = radius;
+        this.centerX = centerX;
+        this.centerY = centerY;
+        this.rotation = rotation;
+    }
+    rotate(){
+        let delta = 10;
+        this.rotation += delta;
+
+    }
 }
-function draw_circle(radius, centerX, centerY){
+function rotateCircles(circles){
+    let i;
+    clearGraph();
+    for(i=0; i<circles.length; i++){
+        circles[i].rotate();
+        draw_circle(circles[i]);
+    }
+}
+
+function clearGraph(){
+    let c = document.getElementById("graph");
+    let ctx=c.getContext("2d");
+    ctx.clearRect(0,0,600,600);
+}
+
+function setup(){
+    let i;
+    let circles = [];
+    for(i=1; i<=5; i++){
+        circles.push(new Circle(i*10, (6-i)*100,(6-i)*100, i*6));
+    }
+    for(i=0; i<5; i++){
+        draw_circle(circles[i]);
+    }
+    for(i=0; i<5; i++){
+        setTimeout(function(){rotateCircles(circles);}, (i+1)*1000);
+    }
+}
+function draw_circle(circle){
     const radian = Math.PI/180;
     let c = document.getElementById("graph");
     let ctx = c.getContext("2d");
@@ -11,10 +47,13 @@ function draw_circle(radius, centerX, centerY){
     ctx.strokeStyle = "black";
     ctx.beginPath();
     let i;
-    ctx.translate(centerX, centerY);
+    ctx.translate(circle.centerX, circle.centerY);
     for(i=0; i<361; i++){
-        ctx.lineTo((radius*Math.cos(i*radian)), (radius*Math.sin(i*radian)));
+        ctx.lineTo((circle.radius*Math.cos(i*radian)), (circle.radius*Math.sin(i*radian)));
     }
     ctx.stroke();
-    ctx.translate(-centerX,-centerY);
+    ctx.moveTo(0,0);
+    ctx.lineTo((circle.radius*Math.cos(circle.rotation*radian)),circle.radius*Math.sin(circle.rotation*radian));
+    ctx.stroke();
+    ctx.translate(-circle.centerX,-circle.centerY);
 }
